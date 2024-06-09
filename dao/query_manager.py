a5a427c6d9b8e5c4eb648cadfc1af591defbb084
Shamit Verma <github@shamit.in>
2024-06-09 20:47:43 +0530

    Added code to generate queries and save status in a SQLite db

---
+++ dao/query_manager.py
@@ -0,0 +1,63 @@
+# Python file: query_manager.py
+import sqlite3
+
+
+class QueryManager:
+    def __init__(self, db_path):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+
+        # Create table if it doesn't exist
+        self.cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS queries 
+                    (   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Entity TEXT NOT NULL,
+                        start_time REAL,
+                        end_time REAL,
+                        query_text TEXT,
+                        extracted_query TEXT,
+                        task_done BOOLEAN DEFAULT FALSE
+                    )
+                ''')
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+    def insert_query(self, entity, start_time, end_time, llm_output, extracted_query, task_done):
+        cur = self.conn.cursor()
+        query = """
+            INSERT INTO queries 
+            (Entity, start_time, end_time, llm_output, extracted_query, task_done)
+            VALUES (?, ?, ?, ?, ?, ?);
+        """
+        cur.execute(query, (entity, start_time, end_time, llm_output, extracted_query, task_done))
+        self.conn.commit()
+        return cur.lastrowid
+
+    def get_unfinished_task(self):
+        cur = self.conn.cursor()
+        query = """
+            SELECT * FROM queries 
+            WHERE task_done = 0 
+            ORDER BY ID ASC 
+            LIMIT 1;    
+        """
+        cur.execute(query)
+        row = cur.fetchone()
+
+        if row is None:
+            return None
+        col_names = [desc[0] for desc in cur.description]
+        return dict(zip(col_names, row))
+
+    def get_by_id(self, id):
+        cur = self.conn.cursor()
+        query = "SELECT * FROM queries WHERE ID = ?"
+        cur.execute(query, (id,))
+        row = cur.fetchone()
+
+        if row is None:
+            return None
+        col_names = [desc[0] for desc in cur.description]
+        return dict(zip(col_names, row))
--- tests/phi_3_sql_hello_world.py
+++ tests/phi_3_sql_hello_world.py
@@ -1,7 +1,19 @@
+import logging
+import time
+
 from langchain_community.llms import LlamaCpp
 from langchain_community.utilities.sql_database import SQLDatabase
 from sqlalchemy import create_engine
+from dao import QueryManager
 
+# create QueryManager instance
+query_manager = QueryManager("sql_stats.sqlite")
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 prompt = """### Task
 Generate a SQL query to answer [QUESTION]{question}[/QUESTION]
@@ -22,21 +34,38 @@ Given the database schema, here is the SQL query that answers [QUESTION]{questio
 
 db_file_path = '../data/Chinook_Sqlite.sqlite'
 db_uri = "sqlite:///" + db_file_path
-db = SQLDatabase.from_uri(db_uri,sample_rows_in_table_info=0)
+db = SQLDatabase.from_uri(db_uri, sample_rows_in_table_info=0)
 db_engine = create_engine(db_uri)
 db_schema_str = db.get_table_info()
 
 llm_file_path = '../models/Phi-3-mini-4k-instruct-q4.gguf'
 
 llm = LlamaCpp(
-        model_path=llm_file_path,
-        n_ctx=4096,
-        temperature=0,
-        seed=4381,
-        max_tokens=4000
-    )
+    model_path=llm_file_path,
+    n_ctx=4096,
+    temperature=0,
+    seed=4381,
+    max_tokens=4000,
+    verbose=False,
+    n_gpu_layers=1
+)
 
-inp = prompt.format(question="Which customer generated max sales?",db_schema=db_schema_str)
-output = llm.invoke(inp)
+entities = ['client', 'customer', 'artist', 'track', 'album', 'genre', 'playlist', 'salesperson', 'employee']
 
-print(output)
+# Specify the path to the SQLite database
+database_path = "../data/query_tasks.sqlite"
+
+# Create QueryManager instance
+query_manager = QueryManager(database_path)
+
+for entity in entities:
+    start_time = time.time()
+    qry = "Which " + entity + " generated max sales?"
+    logger.info(qry)
+
+    inp = prompt.format(question=qry, db_schema=db_schema_str)
+    output = llm.invoke(inp)
+    end_time = time.time()
+    query_manager.insert_query(entity=entity, start_time=start_time, end_time=end_time, llm_output=output,
+                               task_done=False, extracted_query=None)
+    #print(output)
